f5fbdbbd5c93d57e58e3924e627c68410879bd70
Tatsuya Yamamoto <tatsuya@ymt2.net>
2013-01-10 01:19:21 +0900

    add

---
+++ create_symlinks.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+for dotfile in .?*;
+do
+    case $dotfile in
+	.DS_Store | .git | .gitignore | dot.global.gitignore)
+	    continue;;
+	*)
+	    ln -is "$PWD/$dotfile" $HOME
+	    ;;
+    esac
+done
+
+ln -is "$PWD/dot.global.gitignore" $HOME/.gitignore
