4ae7dab5e459a27b3eb214a4816b6dbb32f7fffb
Tatsuya Yamamoto <tatsuya@ymt2.net>
2013-03-02 23:02:53 +0900

    fix

--- .emacs.d/inits/10_flex_autopair.el
+++ .emacs.d/inits/10_flex_autopair.el
@@ -2,5 +2,5 @@
 ;;
 ;; (auto-install-from-url "https://raw.github.com/uk-ar/flex-autopair/0184a539b874e20938418e4dbeaec0665129bf46/flex-autopair.el")
 
-(require 'flex-autopair)
-(flex-autopair-mode 1)
+;(require 'flex-autopair)
+;(flex-autopair-mode 1)
