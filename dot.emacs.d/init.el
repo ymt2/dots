2e38f6f3a9898bbdd328b00a357253257409da87
Tatsuya Yamamoto <tatsuya@ymt2.net>
2013-07-20 01:33:00 +0900

    refactor init

--- dot.emacs.d/init.el
+++ dot.emacs.d/init.el
@@ -9,9 +9,6 @@
   (setq package-user-dir (concat user-emacs-directory "elpa"))
   (package-initialize))
 
-;; auto-install
-;; (package-install 'auto-install)
-
 ;; load-path
 (let ((default-directory (expand-file-name "~/.emacs.d/site-lisp")))
   (add-to-list 'load-path default-directory)
@@ -19,14 +16,24 @@
       (normal-top-level-add-subdirs-to-load-path)))
 (add-to-list 'load-path "~/.emacs.d/auto-install")
 
-;; info
-(require 'info)
-(add-to-list 'Info-additional-directory-list "~/.emacs.d/info")
+;; auto-install
+(cond ((require 'auto-install nil t)
+       (setq auto-install-directory "~/.emacs.d/auto-install")
+       (auto-install-update-emacswiki-package-name t)
+       (auto-install-compatibility-setup)) ; for compatibility
+      (package-install 'auto-install))
 
 ;; init-loader
-;; (package-install 'init-loader)
-(when (require 'init-loader nil t)
-  (init-loader-load "~/.emacs.d/inits"))
+(cond ((require 'init-loader nil t)
+       (setq-default init-loader-byte-compile t)
+					;(setq-default init-loader-show-log-after-init nil
+					;init-loader-byte-compile t)
+       (init-loader-load "~/.emacs.d/inits"))
+      (package-install 'init-loader))
+
+;; info
+(when (require 'info nil t)
+  (add-to-list 'Info-additional-directory-list "~/.emacs.d/info"))
 
 ;; auto byte compile
 (add-hook 'after-save-hook
--- dot.emacs.d/inits/40_auto_install.el
+++
@@ -1,6 +0,0 @@
-;; auto-install
-(require 'auto-install)
-(setq auto-install-directory "~/.emacs.d/auto-install")
-(auto-install-update-emacswiki-package-name t)
-; for compatibility
-(auto-install-compatibility-setup)
