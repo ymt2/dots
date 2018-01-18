b96db5a83d1d6ee9d39dbd4c6618d8bf4baac06d
ymt2 <tatsuya@ymt2.net>
2018-01-18 18:28:44 +0900

    Fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,19 @@
 #! /bin/sh
 
+function log() {
+    local fname="$(cd "$(dirname "${BASH_SOURCE:-${(%):-%N}}")" && pwd)"
+    echo "$(date '+%Y-%m-%dT%H:%M:%S') ${PROCNAME} (${fname}:${BASH_LINENO[0]}:${FUNCNAME[1]}) $@"
+}
+
 for dot in dot.?*;
 do
     file_name=`echo $dot | sed -e "s/^dot\./\./"`
+
+    if [ -d $HOME/$file_name ]; then
+        log "skip: ${HOME}/${file_name}"
+        continue
+    fi
+
     ln -is $PWD/$dot $HOME/$file_name
 done
 
