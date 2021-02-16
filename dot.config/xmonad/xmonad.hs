e4b31bfa9d1eb306a6975c10dc29006a2c84c709
ymt2 <tatsuya@ymt2.net>
2021-02-16 10:45:53 +0900

    Screenshot

--- dot.config/xmonad/xmonad.hs
+++ dot.config/xmonad/xmonad.hs
@@ -1,13 +1,23 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops -- For support wmctrl
 import XMonad.Hooks.SetWMName
+import XMonad.Util.SpawnOnce (spawnOnce)
+import XMonad.Util.EZConfig (additionalKeys)
+
+superMask = mod4Mask
+altMask = mod1Mask
 
 main :: IO()
 main = do
   xmonad $ ewmh defaultConfig
     { terminal = "kitty"
     , startupHook = myStartupHook
-    }
+    } `additionalKeys`
+    [ ((0,           xK_Print), spawn "xfce4-screenshooter")
+    , ((controlMask, xK_Print), spawn "xfce4-screenshooter -r")
+    ]
 
 myStartupHook = do
   setWMName "LG3D" -- For support IntelliJ idea and more.
+  spawnOnce "xfce4-clipman &"
