951fd885697b380a50e27b048a59160f9052c435
ymt2 <tatsuya@ymt2.net>
2021-02-23 16:26:32 +0900

    spawnOnce

--- dot.config/xmonad/xmonad.hs
+++ dot.config/xmonad/xmonad.hs
@@ -41,5 +41,5 @@ myLogHook h = dynamicLogWithPP xmobarPP
 
 myStartupHook = do
   setWMName "LG3D" -- For support IntelliJ idea and more.
-  spawnOnce "dropbox"
-  spawnOnce "xfce4-clipman"
+  spawnOnce "dropbox &"
+  spawnOnce "xfce4-clipman &"
